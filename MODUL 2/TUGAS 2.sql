734aad5f2b05e83cd5d7feaa7cd17f641d40071a
Yostyawan <revantio759@gmail.com>
2026-05-07 12:05:13 +0800

    Add files via upload

---
+++ MODUL 2/TUGAS 2.sql
@@ -0,0 +1,20 @@
+-- Nama  : Tio agus setiawan
+-- kelas : A
+-- modul : Tugas 2
+
+-- Menggunakan database
+USE Pti_mart;
+
+-- Menampilkan database
+SHOW DATABASES;
+
+-- Cek isi Tabel
+SELECT * FROM pelanggan;
+SELECT * FROM produk;
+
+
+-- case 1 tim sales mencari nama customer dan alamatnya
+SELECT p.nama_pelanggan AS nama_customer, p.alamat AS alamat_customer FROM pelanggan p;
+
+-- case 2 tim sales ingin melihat nama produk dan harganya
+SELECT pr.nama_produk , pr.harga AS harga_produk FROM produk pr;
